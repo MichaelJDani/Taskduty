6a16688edebcf115eccff8f196ce58b81e1a3ba8
MichaelJDani <Ozordanielmichael@gmail.com>
2026-03-14 13:14:54 +0100

    fixes

--- client/src/pages/Hero.jsx
+++ client/src/pages/Hero.jsx
@@ -3,13 +3,23 @@ import { useNavigate } from "react-router-dom";
 export default function Hero() {
   const navigate = useNavigate();
 
+  const handleGoToTasks = () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      
+      navigate("/login");
+      return;
+    }
+
+    
+    navigate("/tasks");
+  };
+
   return (
     <main className="bg-gray-50 py-8 px-6 md:px-20 mt-10 overflow-hidden">
-      
       <div className="container mx-auto grid md:grid-cols-2 items-center gap-10 md:gap-15 py-10 md:py-20">
-        
         <div className="flex flex-col gap-6 text-center md:text-left">
-          
           <h1 className="text-4xl md:text-5xl font-bold text-dark-purple leading-tight md:w-120">
             Manage your Tasks on{" "}
             <span className="text-custom-hover">TaskDuty</span>
@@ -24,13 +34,12 @@ export default function Hero() {
 
           <button
             className="mt-4 w-full md:w-max bg-custom-hover hover:bg-purple-700 text-white px-6 py-3 rounded-lg shadow-md transition cursor-pointer"
-            onClick={() => navigate("/tasks")}
+            onClick={handleGoToTasks}
           >
             Go to My Tasks
           </button>
         </div>
 
-        
         <div className="flex justify-center md:justify-end">
           <img
             src="/hero_image.png"
@@ -38,7 +47,6 @@ export default function Hero() {
             className="w-full max-w-sm md:max-w-md"
           />
         </div>
-
       </div>
     </main>
   );
