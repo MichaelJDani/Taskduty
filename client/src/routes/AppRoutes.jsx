e7870c379a9cca04b0617f12f0b6c2a1f38adac7
MichaelJDani <Ozordanielmichael@gmail.com>
2026-03-19 10:27:23 +0100

    added note  page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,14 +3,15 @@ import { lazy, Suspense } from "react";
 import MainLayout from "../layouts/MainLayout";
 import LazyLoader from "../components/LazyLoader";
 
-
 const Hero = lazy(() => import("../pages/Hero"));
 const Tasks = lazy(() => import("../pages/Tasks"));
 const NewTask = lazy(() => import("../pages/NewTask"));
 const EditTask = lazy(() => import("../pages/EditTask"));
 const Login = lazy(() => import("../pages/Login"));
 const Register = lazy(() => import("../pages/Register"));
-
+const Notes = lazy(() => import("../pages/Notes"));
+const NewNote = lazy(() => import("../pages/NewNote"));
+const EditNote = lazy(() => import("../pages/EditNote"));
 
 function withSuspense(Component) {
   return (
@@ -32,6 +33,9 @@ export default function AppRoutes() {
         { path: "tasks/:id/edit", element: withSuspense(EditTask) },
         { path: "login", element: withSuspense(Login) },
         { path: "register", element: withSuspense(Register) },
+        { path: "notes", element: withSuspense(Notes) },
+        { path: "notes/new", element: withSuspense(NewNote) },
+        { path: "notes/:id/edit", element: withSuspense(EditNote) },
       ],
     },
   ]);
